5833e1dbd46aab196d0cd44b7d7686d89ed3dd3d
austinlrq <47692597+austinlrq@users.noreply.github.com>
2024-11-06 18:27:02 +0100

    Add files via upload

---
+++ Jeu_Mediavale/src/models/personnes/__init__.py
@@ -0,0 +1,5 @@
+from .personne import Personne
+from .roturier import Roturier
+from .paysan import Paysan
+from .noble import Noble
+from .seigneur import Seigneur
---
+++ Jeu_Mediavale/src/models/personnes/noble.py
@@ -0,0 +1,32 @@
+from .personne import Personne
+from .roturier import Roturier
+from typing import List
+
+class Noble(Personne):
+    """
+    Classe représentant un noble, ayant des roturiers comme sujets.
+    """
+
+    def __init__(self, nom: str, age: int, ressources: int, argent: int):
+        super().__init__(nom, age, ressources, argent)
+        self.roturiers: List[Roturier] = []
+
+    def ajouter_roturier(self, roturier: Roturier):
+        """Ajoute un roturier à la liste des roturiers du noble."""
+        self.roturiers.append(roturier)
+
+    def percevoir_impot(self):
+        """
+        Perçoit un impôt en prenant une part des ressources de chaque roturier.
+        """
+        for roturier in self.roturiers:
+            impots = roturier.ressources // 2  # Par exemple, le noble prend 50 % des ressources du roturier
+            roturier.diminuer_ressources(impots)
+            self.augmenter_ressources(impots)
+
+    def __str__(self):
+        return (
+            super().__str__() +
+            f", Type : Noble, "
+            f"Nombre de roturiers : {len(self.roturiers)}"
+        )
